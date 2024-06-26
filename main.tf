c70d807ce9e4e47a4ba3284e1ebd22848860fb96
Rem Baumann <baumann.rem@gmail.com>
2024-06-26 18:49:31 +0000

    0.5.0 Terraform Provider Init Plan Apply

--- main.tf
+++ main.tf
@@ -1,1 +1,26 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.6.2"
+    }
+  }
+}
 
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "bucket_name" {
+  length           = 16
+  special          = false
+  override_special = ""
+}
+
+output "random_bucket_name_id" {
+    value =  random_string.bucket_name.id
+}
+
+output "random_bucket_name_result" {
+    value =  random_string.bucket_name.result
+}
