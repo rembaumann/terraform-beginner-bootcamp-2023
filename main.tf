c35cf151c1362e744c138e07485a7c5d87b51a6c
Rem Baumann <baumann.rem@gmail.com>
2024-06-27 21:59:27 +0000

    terraform random bucket creator

--- main.tf
+++ main.tf
@@ -4,7 +4,17 @@ terraform {
       source = "hashicorp/random"
       version = "3.6.2"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.55.0"
+    }
   }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
 }
 
 provider "random" {
@@ -13,12 +23,13 @@ provider "random" {
 
 resource "random_string" "bucket_name" {
   length           = 16
+  lower            = true
+  upper            = false
   special          = false
-  override_special = ""
 }
 
-output "random_bucket_name_id" {
-    value =  random_string.bucket_name.id
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name_result" {
